ff9cf1bdd0a31f89e1c136fb6585dbc9a977be26
Lotte Eliasson <lottefacereliasson@hotmail.com>
2024-04-22 10:26:38 +0200

    Heuristic

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,9 +1,11 @@
 import Board.Board;
 import Mechanics.Move;
+import Pieces.Piece;
 
 public class Game {
     static Move move;
     static Board board;
+    static Piece piece;
 
     public static void main(String[] args) {
         board = new Board();
@@ -17,7 +19,13 @@ public class Game {
             move.movePiece1(board, true);
             //AI do black stuff
             //AI.move(board, false);
+            move.movePiece(board, false);
+
+            piece.printHeuristicValues(board);
             move.movePiece1(board, false);
         }
     }
+
+
+
 }
--- src/main/java/Pieces/Piece.java
+++ src/main/java/Pieces/Piece.java
@@ -1,6 +1,7 @@
 package Pieces;
 
 import java.util.ArrayList;
+import Board.Board;
 
 public abstract class Piece {
     public boolean isWhite;
@@ -19,4 +20,17 @@ public abstract class Piece {
 
     public abstract int getHeuristicValue();
 
-}
+
+    public static void printHeuristicValues(Board board) {
+        ArrayList<ArrayList<Piece>> boardArray = board.getBoard();
+        for (int i = 0; i < boardArray.size(); i++) {
+            for (int j = 0; j < boardArray.get(i).size(); j++) {
+                Piece piece = boardArray.get(i).get(j);
+                if (piece != null) {
+                    System.out.println(piece + " at position (" + i + "," + j +
+                            ") has heuristic value: " + piece.getHeuristicValue());
+                }
+            }
+        }
+    }
+    }
